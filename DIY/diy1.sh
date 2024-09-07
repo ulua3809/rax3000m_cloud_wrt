6ea49b8de2bc2613126f16c3437e0dfe596d8944
ulua3809 <63995396+ulua3809@users.noreply.github.com>
2024-09-07 14:37:42 +0800

    using self extra ipk

--- DIY/diy1.sh
+++ DIY/diy1.sh
@@ -5,4 +5,4 @@
 
 
 ##添加自己的插件库
-echo -e "\nsrc-git extraipk https://github.com/mzwrt/extra_ipk" >> feeds.conf.default
+echo -e "\nsrc-git extraipk https://github.com/ulua3809/ulua_extra_ipk" >> feeds.conf.default
